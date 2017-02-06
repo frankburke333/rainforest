042780fe8b69fe298457db2da5aa184dda404c9e
frankburke333 <frank.burke@gmail.com>
2017-02-06 17:01:23 -0500

    add user creation validation

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
+  get 'users/create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'products#index'
 
 
   resources :products
+  resources :users, only: [:new, :create]
+
 
 end
